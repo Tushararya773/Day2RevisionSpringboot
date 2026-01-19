b62250960a4ec861fa3a0808d4aed90f4fe60705
Tushararya <tushararya773@gmail.com>
2026-01-19 15:07:50 +0530

    2nd commit

--- src/main/java/com/example/day2_revision/controller/home.java
+++ src/main/java/com/example/day2_revision/controller/home.java
@@ -1,12 +1,14 @@
 package com.example.day2_revision.controller;
 
+import com.example.day2_revision.model.student_model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class home {
-    @GetMapping("/about")
-    public String about(){
-        return "This is about page Buddy";
+    @GetMapping("/")
+    public student_model getstudent(){
+        student_model student= new student_model(1,"Tushar","tushararya773@gmail.com");
+        return student;
     }
 }
--- src/main/java/com/example/day2_revision/model/student_model.java
+++ src/main/java/com/example/day2_revision/model/student_model.java
@@ -7,13 +7,15 @@ public class student_model {
     private int id;
     private String name;
     private String email;
-    public void student_model(int id ,String name ,String email ){
+    public student_model(int id ,String name ,String email ){
         this.id=id;
         this.name=name;
         this.email=email;
 
     }
+    public student_model(){
 
+    }
     public int getId() {
         return id;
     }
@@ -37,4 +39,5 @@ public class student_model {
     public void setEmail(String email) {
         this.email = email;
     }
+
 }
